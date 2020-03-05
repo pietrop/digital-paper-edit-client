08f732590ffa682e7aecfee9df7645e70896f3cc
Pietro Passarelli <pietro.passarelli@wsj.com>
2020-03-05 18:18:46 -0500

    Updates

---
+++ precache-manifest.3a33b0f8b7cd3e2fd9c492df4dab1695.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a694cb6ed49200747bdf7f0b8f7563",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6788d42c9d35592ed39d",
+    "url": "./static/css/2.c6250e3a.chunk.css"
+  },
+  {
+    "revision": "a52c54ba8feefa2c693c",
+    "url": "./static/css/main.1a1a325b.chunk.css"
+  },
+  {
+    "revision": "6788d42c9d35592ed39d",
+    "url": "./static/js/2.6f3690c6.chunk.js"
+  },
+  {
+    "revision": "00fc574a814dd289c67dc0af9903bdc2",
+    "url": "./static/js/2.6f3690c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a52c54ba8feefa2c693c",
+    "url": "./static/js/main.f895c7c2.chunk.js"
+  },
+  {
+    "revision": "9c80c73c9d9e2897cab3",
+    "url": "./static/js/runtime-main.7366a0a1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4ae4bf58520f55191741260505cfb77e.js"
+  "./precache-manifest.3a33b0f8b7cd3e2fd9c492df4dab1695.js"
 );
 
 self.addEventListener('message', (event) => {
